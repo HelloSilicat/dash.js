bc05f4e2f62e2bae90e9d246da3a6966fce9704f
HelloSilicat <453715647pjl@gmail.com>
2020-11-28 12:31:10 +0800

    recover switch

--- src/streaming/rules/abr/ABRRulesCollection.js
+++ src/streaming/rules/abr/ABRRulesCollection.js
@@ -32,7 +32,7 @@ import ThroughputRule from './ThroughputRule';
 import InsufficientBufferRule from './InsufficientBufferRule';
 import AbandonRequestsRule from './AbandonRequestsRule';
 //import DroppedFramesRule from './DroppedFramesRule';
-//import SwitchHistoryRule from './SwitchHistoryRule';
+import SwitchHistoryRule from './SwitchHistoryRule';
 import BolaRule from './BolaRule';
 import FactoryMaker from '../../../core/FactoryMaker';
 import SwitchRequest from '../SwitchRequest';
@@ -77,9 +77,9 @@ function ABRRulesCollection(config) {
                     dashMetrics: dashMetrics
                 })
             );
-            //qualitySwitchRules.push(
-            //    SwitchHistoryRule(context).create()
-            //);
+            qualitySwitchRules.push(
+                SwitchHistoryRule(context).create()
+            );
             // qualitySwitchRules.push(
             //     DroppedFramesRule(context).create()
             // );
@@ -157,7 +157,7 @@ function ABRRulesCollection(config) {
         const maxQuality = getMinSwitchRequest(activeRules);
 
         // For BUPT Trace
-        if (switchRequestArray && switchRequestArray.length == 4) {
+        if (switchRequestArray && switchRequestArray.length == 5) {
             const mediaType = rulesContext.getMediaType();
             const abrController = rulesContext.getAbrController();
             const streamInfo = rulesContext.getStreamInfo();
